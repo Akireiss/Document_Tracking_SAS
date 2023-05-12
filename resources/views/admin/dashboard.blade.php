01752f4dc9d08a68ee1f444ed1f5fc724c5de666
Joshua Ledda <joshcstnd@gmail.com>
2023-05-12 09:22:30 +0800

    Commiat1

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -22,7 +22,7 @@
         <div class="row">
 
           <!-- Sales Card -->
-          <div class="col-xxl-3 col-md-6">
+          <div class="col-xxl-4 col-md-6">
             <div class="card info-card sales-card">
 
               <div class="filter">
@@ -39,14 +39,14 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Sales <span>| Today</span></h5>
+                <h5 class="card-title">Total Numbers Of Release</h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                     <i class="bi bi-cart"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>145</h6>
+                    <h6>180</h6>
                     <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                   </div>
@@ -57,7 +57,7 @@
           </div><!-- End Sales Card -->
 
           <!-- Revenue Card -->
-          <div class="col-xxl-3 col-md-6">
+          <div class="col-xxl-4 col-md-6">
             <div class="card info-card revenue-card">
 
               <div class="filter">
@@ -74,14 +74,14 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Revenue <span>| This Month</span></h5>
+                <h5 class="card-title">Total Of Incoming Vouchers</h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                     <i class="bi bi-currency-dollar"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>$3,264</h6>
+                    <h6>50</h6>
                     <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                   </div>
@@ -93,7 +93,7 @@
 
          <!-- Customers Card -->
 
-          <div class="col-xxl-3 col-xl-12">
+          <div class="col-xxl-4 col-xl-12">
 
             <div class="card info-card customers-card">
 
@@ -111,14 +111,14 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Customers <span>| This Year</span></h5>
+                <h5 class="card-title">Total Of Outgoing Vouchers</h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                     <i class="bi bi-people"></i>
                   </div>
                   <div class="ps-3">
-                    <h6>1244</h6>
+                    <h6>Voucher</h6>
                     <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
 
                   </div>
@@ -134,43 +134,7 @@
 
 
 
-          <div class="col-xxl-3 col-xl-12">
 
-            <div class="card info-card customers-card">
-
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Today</a></li>
-                  <li><a class="dropdown-item" href="#">This Month</a></li>
-                  <li><a class="dropdown-item" href="#">This Year</a></li>
-                </ul>
-              </div>
-
-              <div class="card-body">
-                <h5 class="card-title">Customers <span>| This Year</span></h5>
-
-                <div class="d-flex align-items-center">
-                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-people"></i>
-                  </div>
-                  <div class="ps-3">
-                    <h6>1244</h6>
-                    <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                  </div>
-                </div>
-
-              </div>
-            </div>
-
-
-
-          </div>
 
 
           <!-- End Customers Card -->
@@ -184,22 +148,6 @@ $assigned_to = "Mark Johnson";
 $last_updated = "2023-04-09 12:45:00";
 
 ?>
-
-<div class="card border-primary">
-  <div class="card-header bg-primary text-white">
-    Document Tracking
-  </div>
-  <div class="card-body">
-    <h5 class="card-title"><?php echo $document_name; ?></h5>
-    <p class="card-text">
-      <b>Status:</b> <?php echo $document_status; ?><br>
-      <b>Due Date:</b> <?php echo date("M j, Y", strtotime($document_due_date)); ?><br>
-      <b>Assigned To:</b> <?php echo $assigned_to; ?><br>
-      <b>Last Updated:</b> <?php echo date("M j, Y g:i A", strtotime($last_updated)); ?>
-    </p>
-    <a href="#" class="btn btn-primary">View Document</a>
-  </div>
-</div>
 
 
           <!-- Reports -->
@@ -236,11 +184,84 @@ $last_updated = "2023-04-09 12:45:00";
                     </ul>
                   </div>
 
-                  <div class="card-body">
-                    <h5 class="card-title">Reports <span>/Today</span></h5>
+                  {{-- <div class="card-body">
+                    <h5 class="card-title">Reports <span>/Today</span></h5> --}}
 
-<!-- Line Chart -->
-<div id="reportsChart"></div>
+
+
+
+
+
+                    <div class="col-lg-6">
+                        <div class="card">
+                          <div class="card-body">
+                            <h5 class="card-title">Bar CHart</h5>
+
+                            <!-- Bar Chart -->
+                            <canvas id="barChart" style="max-height: 400px;"></canvas>
+                            <script>
+                              document.addEventListener("DOMContentLoaded", () => {
+                                new Chart(document.querySelector('#barChart'), {
+                                  type: 'bar',
+                                  data: {
+                                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                                    datasets: [{
+                                      label: 'Bar Chart',
+                                      data: [65, 59, 80, 81, 56, 55, 40],
+                                      backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 205, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(201, 203, 207, 0.2)'
+                                      ],
+                                      borderColor: [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(255, 159, 64)',
+                                        'rgb(255, 205, 86)',
+                                        'rgb(75, 192, 192)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(153, 102, 255)',
+                                        'rgb(201, 203, 207)'
+                                      ],
+                                      borderWidth: 1
+                                    }]
+                                  },
+                                  options: {
+                                    scales: {
+                                      y: {
+                                        beginAtZero: true
+                                      }
+                                    }
+                                  }
+                                });
+                              });
+                            </script>
+                            <!-- End Bar CHart -->
+
+                          </div>
+                        </div>
+                      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @push('script')
 
