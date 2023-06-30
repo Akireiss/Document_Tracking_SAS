7da77952c0699a62f67b350464abb040d9356ed4
Joshua Ledda <joshcstnd@gmail.com>
2023-06-30 15:12:22 +0800

    ledda

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -39,6 +39,7 @@
             </a>
           </li>
 
+
         </ul>
       </li>
       <!-- End Forms Nav -->
@@ -90,6 +91,26 @@
 
         </ul>
 
+
+<br>
+
+        <li class="nav-item">
+          <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#" aria-expanded="false">
+            <i class="bi bi-person-plus-fill"></i><span>Add Account</span><i class="bi bi-chevron-down ms-auto"></i>
+          </a>
+          <ul id="tables-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav" style="">
+            <li>
+              <a href="tables-general.html">
+                <i class="bi bi-circle"></i><span>Create Account</span>
+              </a>
+            </li>
+            <li>
+              <a href="tables-data.html">
+                <i class="bi bi-circle"></i><span> Edit Account</span>
+              </a>
+            </li>
+          </ul>
+        </li>
 <br>
     <li>
         <a class="nav-link collapsed" href="pages-Logout.html">
